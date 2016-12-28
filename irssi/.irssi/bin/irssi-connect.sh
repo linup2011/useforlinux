6a651280a87c0b5dab3a6a1119abfbf5cb236055
Jianlin Shi <jishi@redhat.com>
2016-12-28 12:59:16 +0800

    add config and scripts for irssi

---
+++ irssi/.irssi/bin/irssi-connect.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# If we get an argument, use it for ssh port, otherwise use default of 22
+if [ -n "$1" ]
+then
+    port=$1
+else
+    port=22
+fi
+
+host="10.66.13.139"
+notify="$HOME/bin/notify-remote.sh"
+
+set -e
+
+socat -u tcp4-listen:12000,reuseaddr,fork,bind=127.0.0.1 exec:$notify &
+
+# If you only have one remote screen session
+#autossh $host -p $port -R 12000:localhost:12000 -t 'screen -r -D'
+
+# Attaches to 'irc' screen session
+# autossh $host -p $port -R 12000:localhost:12000 -t 'tmux attach -t irssi'
+ssh -X lin@$host -R 12000:localhost:12000
+
+kill %1
